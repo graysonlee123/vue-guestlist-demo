2e453b280777f400c29ad484aec6b40a1172cd44
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-26 19:35:19 -0400

    vue basics

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     event: {
-      date: 'August 14th - 16th',
-      title: 'Summer Festival!',
-      description: 'It\'s back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
+      date: "August 14th - 16th",
+      title: "Summer Festival!",
+      description:
+        "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+    },
+    newNameText: "",
+    guestName: [],
+    appStyles: {
+      marginTop: "25px"
+    },
+    eventCapacity: 25,
+    eventCapacityPercentage: 0
+  },
+  methods: {
+    formSubmitted: function () {
+      if (this.newNameText.length > 0 && this.eventCapacityPercentage < 100) {
+        this.guestName.push(this.newNameText);
+        this.newNameText = "";
+        this.eventCapacityPercentage =
+          this.guestName.length / (this.eventCapacity / 100);
+      }
+    }
+  },
+  computed: {
+    sortNames: function() {
+      return this.guestName.sort();
     }
   }
 });
